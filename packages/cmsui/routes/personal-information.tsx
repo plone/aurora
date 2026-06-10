be7f2465a2a282e9b7890234b258f8fbbeb0f348
Tibor Szakmany <szakmany@interaktiv.de>
2026-06-10 15:14:11 +0200

    limit @@personal-information to logged in users (#117)

--- packages/cmsui/routes/personal-information.tsx
+++ packages/cmsui/routes/personal-information.tsx
@@ -1,3 +1,13 @@
+import { RouterContextProvider, type LoaderFunctionArgs } from 'react-router';
+import { requireAuthCookie } from '@plone/react-router';
+
+export async function loader({
+  request,
+}: LoaderFunctionArgs<RouterContextProvider>) {
+  await requireAuthCookie(request);
+  return {};
+}
+
 export default function PersonalInformation() {
   return <h1 className="documentFirstHeading">Personal Information</h1>;
 }
