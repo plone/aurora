1019fa95ee391372223d13a276cee0edd23982d0
Tibor Szakmany <szakmany@interaktiv.de>
2026-06-10 15:30:01 +0200

    fetch user data (#117)

--- packages/cmsui/routes/personal-information.tsx
+++ packages/cmsui/routes/personal-information.tsx
@@ -1,13 +1,26 @@
-import { RouterContextProvider, type LoaderFunctionArgs } from 'react-router';
+import {
+  RouterContextProvider,
+  useLoaderData,
+  type LoaderFunctionArgs,
+} from 'react-router';
 import { requireAuthCookie } from '@plone/react-router';
+import { ploneUserContext } from '@plone/aurora/app/middleware.server';
 
 export async function loader({
   request,
+  context,
 }: LoaderFunctionArgs<RouterContextProvider>) {
   await requireAuthCookie(request);
-  return {};
+  const user = context.get(ploneUserContext);
+  return { user };
 }
 
 export default function PersonalInformation() {
-  return <h1 className="documentFirstHeading">Personal Information</h1>;
+  const { user } = useLoaderData<typeof loader>();
+  return (
+    <>
+      <h1 className="documentFirstHeading">Personal Information</h1>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+    </>
+  );
 }
