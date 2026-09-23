f3c687b9ce6b54a10986aa84f0eadbf568d3bc94
Piero Nicolli <piero.nicolli@redturtle.it>
2026-09-23 16:59:40 +0200

    Updated for aurora

--- packages/layout/views/NewsItemView.tsx
+++ packages/layout/views/NewsItemView.tsx
@@ -1,6 +1,5 @@
-import config from '@plone/registry';
 import RenderBlocks from '../blocks/RenderBlocks';
-import type { RootLoader } from 'seven/app/root';
+import type { RootLoader } from '@plone/aurora/app/root';
 import { Container } from '@plone/components';
 import { hasBlocksData } from '@plone/helpers';
 import { useRouteLoaderData } from 'react-router';
@@ -8,41 +7,23 @@ import { useRouteLoaderData } from 'react-router';
 export default function DefaultView() {
   const rootData = useRouteLoaderData<RootLoader>('root');
 
-  if (!rootData) {
+  if (!rootData || rootData.content['@type'] !== 'News Item') {
     return null;
   }
 
   const { content } = rootData;
-
-  const Image = config.getComponent({ name: 'Image' }).component;
+  const hasBlocks = hasBlocksData(content);
 
   return (
     <Container width="default">
-      {content.title && (
-        <h1 className="documentFirstHeading">{content.title}</h1>
-      )}
-
-      {content.description && (
-        <p className="documentDescription">{content.description}</p>
-      )}
-
-      {content.image && (
-        <Image
-          className="documentImage ui right floated image"
-          alt={content.title}
-          title={content.title}
-          item={content}
-          imageField="image"
-          responsive={true}
-        />
-      )}
-
-      {hasBlocksData(content) && (
-        <RenderBlocks
-          content={content}
-          blocksConfig={config.blocks.blocksConfig}
-          pathname="/"
-        />
+      {hasBlocks && <RenderBlocks content={content} />}
+      {!hasBlocks && (
+        <>
+          <h1 className="documentFirstHeading">{content.title}</h1>
+          {Boolean(content.description) && (
+            <p className="documentDescription">{content.description}</p>
+          )}
+        </>
       )}
     </Container>
   );
